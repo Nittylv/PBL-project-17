0c483c0d3ec3c5448ff9e98d0c1d0c1ce0332492
benita_lv@yahoo.com <85848719+Nittylv@users.noreply.github.com>
2022-08-04 01:42:21 -0400

    Update tooling.sh

--- tooling.sh
+++ tooling.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 mkdir /var/www/
 sudo mount -t efs -o tls,accesspoint=fsap-01c13a4019ca59dbe fs-8b501d3f:/ /var/www/
-yum install -y httpd 
+yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 yum module reset php -y
@@ -19,8 +18,6 @@ touch healthstatus
 sed -i "s/$db = mysqli_connect('mysql.tooling.svc.cluster.local', 'admin', 'admin', 'tooling');/$db = mysqli_connect('acs-database.cdqpbjkethv0.us-east-1.rds.amazonaws.com ', 'ACSadmin', 'admin12345', 'toolingdb');/g" functions.php
 chcon -t httpd_sys_rw_content_t /var/www/html/ -R
 systemctl restart httpd
-
-
 
 
 
