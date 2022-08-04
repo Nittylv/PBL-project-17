b9b471f136b8b1fa2906677222f5bc756698221e
benita_lv@yahoo.com <85848719+Nittylv@users.noreply.github.com>
2022-08-04 01:43:31 -0400

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir /var/www/
 sudo mount -t efs -o tls,accesspoint=fsap-0f9364679383ffbc0 fs-8b501d3f:/ /var/www/
-yum install -y httpd 
+yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 yum module reset php -y
@@ -17,12 +17,13 @@ mkdir /var/www/html/
 cp -R /wordpress/* /var/www/html/
 cd /var/www/html/
 touch healthstatus
-sed -i "s/localhost/acs-database.cdqpbjkethv0.us-east-1.rds.amazonaws.com/g" wp-config.php 
-sed -i "s/username_here/ACSadmin/g" wp-config.php 
-sed -i "s/password_here/admin12345/g" wp-config.php 
-sed -i "s/database_name_here/wordpressdb/g" wp-config.php 
+sed -i "s/localhost/acs-database.cdqpbjkethv0.us-east-1.rds.amazonaws.com/g" wp-config.php
+sed -i "s/username_here/ACSadmin/g" wp-config.php
+sed -i "s/password_here/admin12345/g" wp-config.php
+sed -i "s/database_name_here/wordpressdb/g" wp-config.php
 chcon -t httpd_sys_rw_content_t /var/www/html/ -R
 systemctl restart httpd
+
 
 
 
